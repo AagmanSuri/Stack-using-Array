8bcd09769a0b6446239a8c4ca3c87c009685fca6
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2020-12-01 00:37:51 +0530

    structure

--- StackArray/main.c
+++ StackArray/main.c
@@ -1,14 +1,9 @@
-//
-//  main.c
-//  StackArray
-//
-//  Created by Aagman Suri on 01/12/20.
-//
-
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
-}
+#include <stdlib.h>
+struct stack
+{
+    int size;
+    int top;
+    int *S;
+};
+ 
